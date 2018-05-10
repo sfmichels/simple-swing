1ccfb3c4adbcc30f4352a5962d33cb6832ce8182
sfmichels <sf.michels@gmail.com>
2018-05-10 10:56:41 -0700

    Use readable names, better spacing and better indentation.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+# JavaPoint Swing Tutorial #2
 # simple-swing
+# Let's see a simple swing example where we are creating one button and adding it 
+# on the JFrame object inside the main() method.
--- Simple.java
+++ Simple.java
@@ -1,20 +1,20 @@
 import javax.swing.*;  
 public class Simple {  
-JFrame f;  
-Simple(){  
-f=new JFrame();//creating instance of JFrame  
+  JFrame frame;  
+  Simple() {  
+    frame = new JFrame(); //creating instance of JFrame  
           
-JButton b=new JButton("click");//creating instance of JButton  
-b.setBounds(130,100,100, 40);  
+    JButton button = new JButton("click");  //creating instance of JButton  
+    button.setBounds(130,100,100, 40);  
           
-f.add(b);//adding button in JFrame  
+    frame.add(button);  //adding button in JFrame  
           
-f.setSize(400,500);//400 width and 500 height  
-f.setLayout(null);//using no layout managers  
-f.setVisible(true);//making the frame visible  
-}  
+    frame.setSize(400,500);  //400 width and 500 height  
+    frame.setLayout(null);   //using no layout managers  
+    frame.setVisible(true);  //making the frame visible  
+  }  
   
-public static void main(String[] args) {  
-new Simple();  
-}  
+  public static void main(String[] args) {  
+    new Simple();  
+  }  
 }  
